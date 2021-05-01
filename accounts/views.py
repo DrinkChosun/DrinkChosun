9d3e34f9deae9fdf596a94d7703997472537515b
RYS <ryeongseok1125@gmail.com>
2021-05-01 19:25:33 +0900

    Create urlpatterns, views.py function

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+def signup(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def delete(request):
+    pass
+
+
+def update(request):
+    pass
+
+
+def change_password(request):
+    pass
