e9fb713ec3c29f001c14f5f16a27e11197d33ba0
RYS <ryeongseok1125@gmail.com>
2021-05-02 04:43:08 +0900

    Add http decorators at views.py

--- articles/urls.py
+++ articles/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/update/', views.update, name='update'),
     path('<int:pk>/delete/', views.delete, name='delete'),
-    path('<int:pk>/comment/', views.comment_create, name='comment_create'),
+    path('<int:pk>/comment/', views.comments_create, name='comments_create'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,24 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
 from articles.models import Article
 from articles.forms import ArticleForm, CommentForm
+from django.views.decorators.http import require_safe, require_POST, require_http_methods
 
 # Create your views here.
+@require_safe
 def index(request):
-    articles = Article.objects.all()
+    articles = get_list_or_404(Article)
     context = {
         'articles': articles
     }
     return render(request, 'articles/index.html', context)
 
-
+@require_safe
 def detail(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article, pk=pk)
     context = {
         'article': article
     }
     return render(request, 'articles/detail.html', context)
 
-
+@require_http_methods(['GET','POST'])
 def create(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST, request.FILES)
@@ -32,9 +34,9 @@ def create(request):
     }
     return render(request, 'articles/form.html', context)
 
-
+@require_http_methods(['GET','POST'])
 def update(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article, pk=pk)
     if request.method == 'POST':
         form = ArticleForm(request.POST, request.FILES, instance=article)
         if form.is_valid():
@@ -47,15 +49,15 @@ def update(request, pk):
     }
     return render(request, 'articles/form.html', context)
 
-
+@require_POST
 def delete(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article, pk=pk)
     article.delete()
     return redirect('articles:index')
 
-
-def comment_create(request,pk):
-    article = Article.objects.get(pk=pk)
+@require_POST
+def comments_create(request, pk):
+    article = get_object_or_404(Article, pk=pk)
     form = CommentForm(request.POST)
     if form.is_valid():
         comment = form.save(commit=False)
