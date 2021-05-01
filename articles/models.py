f3639743fe7262ca5fdb536874d3453d23d970c6
RYS <ryeongseok1125@gmail.com>
2021-05-02 07:10:41 +0900

    Update image add function in article

--- articles/models.py
+++ articles/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    image = models.ImageField(blank=True)
+    image = models.ImageField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.CharField(max_length=20, default='anonymous')
