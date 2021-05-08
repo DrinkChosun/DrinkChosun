1c308142ae749c1bd82bffc050855849568d9a5f
RYS <ryeongseok1125@gmail.com>
2021-05-08 21:51:20 +0900

    Modify model Article

--- articles/models.py
+++ articles/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
 class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.TextField()
     image = models.ImageField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.CharField(max_length=20, default='anonymous')
+    
 
 class Comment(models.Model):
     content = models.CharField(max_length=200)
-    user = models.CharField(max_length=20, default='anonymous')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
